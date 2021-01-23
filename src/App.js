da6c0ef55e5572359a7ba87d3cd28d0db6b71db3
Cole Starnes <colestarnes1@gmail.com>
2021-01-23 13:56:59 -0600

    mapping

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react"; 
-import axios from "axios"; 
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import EmployeeDetail from "./EmployeeDetail";
 function App() {
- const [employees, setEmployees] = useState([]);
- const [loading, setLoading] = useState(false);
-useEffect(() => {
-  setLoading(true);
-  axios
-  .get("https://randomuser.me/api/?results=10&nat=us")
-  .then(res => { 
-    console.log(res.data.results);
-    setEmployees(...res.data.results);
-    setLoading(false);
-  }).catch(err => {
-    console.log(err);
-  });
-}, []); 
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://randomuser.me/api/?results=10&nat=us")
+      .then(res => {
+        console.log(res.data.results);
+        setEmployees([...res.data.results]);
+        setLoading(false);
+      }).catch(err => {
+        console.log(err);
+      });
+  }, []);
 
-return ( 
-  <div className="App"> 
-  <h1>Employee List</h1>  
-  //m
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Employee List</h1> 
+      <EmployeeDetail employees={employees}/>
+    </div>
+  );
 }
 export default App;
--- src/EmployeeDetail.js
+++ src/EmployeeDetail.js
@@ -1,14 +1,24 @@
-const EmployeeDetail = props => {
-    return (
-        <> 
-        
-            <p>
-                <img src={props.picture.thumbnail} alt={props.name.first} style={{ width: "50px", height: "30px" }} />
-            </p>
-            <p>{props.name.first} {props.name.last}</p>
+import './EmployeeDetail.css'
 
-        </>
-    )
+const EmployeeDetail = (props) => {
+   return (
+       props.employees.map(employee => {
+           console.log(employee)
+           return(  
+             
+               <div> 
+           <h3>{employee.name.first} {employee.name.last} </h3> 
+           <img src={employee.picture.medium}/>
+          <td>
+          <div className="lol">
+           <p>Email: {employee.email}</p> 
+           <p>{employee.phone}</p>
+           </div> 
+           </td>
+           </div>
+           )
+       })
+   )
 }
 
 export default EmployeeDetail;
